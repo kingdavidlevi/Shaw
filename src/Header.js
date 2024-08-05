89204f37cf09d254880459b3cd9c154252b79876
king david levi <125862163+kingdavidlevi@users.noreply.github.com>
2024-08-05 09:02:25 +0100

    first commit

---
+++ src/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { FaRegCommentAlt } from "react-icons/fa";
+function Header(){
+    return(
+        <div className="top-0 flex items-center lg:px-20 xl:px-48 px-2 justify-between fixed h-10 bg-white w-full">
+       <section className="flex gap-6">
+        <section className="flex">
+        <div className="border-l border-t border-b  px-3 grid place-items-center border-gray-300"><a className="text-sm font-normal cursor-pointer hover:text-black  text-blue-700" href="https://www.shaw.ca/contact-us/retail">Personal</a></div>
+        <div className="border px-3 grid place-items-center border-gray-300"><a className="text-sm cursor-pointer hover:text-black font-normal text-blue-700" href="https://business.shaw.ca/">Business</a></div>
+        </section>
+        <section className="md:flex hidden">
+        <div className="  pr-3 grid place-items-center border-blue-700"><a className="text-sm font-normal cursor-pointer hover:text-black  text-blue-700" href="https://www.shaw.ca/store/">Shop</a></div>
+        <div className=" h-3  ml-9 grid place-items-center absolute mt-1.5 bg-black w-half"></div>
+        <div className=" pr-3 grid place-items-center "><a className="text-sm cursor-pointer hover:text-black font-normal text-blue-700" href="https://support.shaw.ca/">Support</a></div>
+        <div className=" h-3  ml-25 grid place-items-center absolute mt-1.5 bg-black w-half"></div>
+        <div className=" grid place-items-center "><a className="text-sm cursor-pointer hover:text-black font-normal text-blue-700" href="https://my.shaw.ca/">My Shaw</a></div>
+        </section>
+       </section>
+       <a className="text-sm cursor-pointer hover:text-black font-normal text-blue-700" href="https://my.shaw.ca/contact-us/"> 
+       <section className="md:flex gap-2 hidden">
+    <FaRegCommentAlt className="text-blue-700 text-base mt-0.5 " />
+<div className="">Contact</div>
+       </section>
+       </a>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/SignUp.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+
+ 
+
+function SignUp() {
+ const [inputs,setInputs] = useState({ email: '', password: '', nameValue: '' })
+
+ const handleFormChanges = (e) =>{
+    const {name, value} = e.target
+
+    setInputs((prevdata) => ({
+      ...prevdata,
+      [name]:value
+     
+
+      
+    }))
+    console.log(inputs)
+ }
+  return (
+    <div className='grid px-3 place-items-center'>
+    <section className="mt-14 pb-10 lg:flex w-full lg:w-310  lg:place-content-start pt-10 bg-white rounded-sm shadow-sm">
+      <div className='lg:w-100  '>
+        <div className='flex justify-center gap-2 items-center'>
+            <section className='mb-6'>
+         <img alt="Shaw Webmail" src='https://webmail.shaw.ca/images/webmail-desktop.png'/>
+        </section>
+        </div>
+        <p className='grid  place-items-center heading'>Sign in to access your Shaw email</p>
+        <form className='sm:grid lg:place-content-start lg:ml-6  ml-4 sm:place-content-center'>
+            <section className='relative'>
+            <div> 
+        <section  className='sm:w-99  w-80%  lg:w-96.5 mt-6 mb-1'><label id="username_input_label" className='user_label  font-semibold' for="username_input">Shaw email</label></section>
+        <input className='username_input_occ  h-10 outline-none border  lg:w-96.5 w-80% sm:w-99'  name="email" type="text"   autocorrect="off" autocapitalize="off" autocomplete="off" tabIndex="1" placeholder="example@shaw.ca"  onChange={handleFormChanges} value={inputs.email}/>
+
+        </div>
+        <a href='#'>
+        <div className='absolute grid place-items-center sm:top-12 sm:mt-1 top-7 sm:-right-11 right-5 '> 
+            <div className='w-4.5 h-4.5 rounded-full grid place-items-center text-white cl1 text-sm font-semibold '><p>?</p></div>
+            <div ><p className='cl font-semibold text-sm'>Help</p></div>
+        </div>
+        </a>
+        </section>
+        <section className='sm:w-99  w-80%  lg:w-96.5 mt-2 mb-1'><label id="username_input_label" className='user_label  font-semibold' for="username_Password">Password</label></section>
+        <input className='username_input_occ h-10 outline-none border lg:w-96.5 w-80%  sm:w-99'  name="password" type="password" value={inputs.password}  onChange={handleFormChanges}  autocorrect="off" autocapitalize="off" autocomplete="off" tabIndex="1" placeholder=""/>
+       <div>
+        <section className='flex items-center gap-2'>
+       <input type="checkbox" id="persistent_check_occ"  className='w-5 mt-4 h-5'   tabindex="4"/>
+       <div><p className='text-gray-500 mt-4 text-base font-normal'>Remember Shaw email</p></div>
+      
+ </section>
+       </div>
+       <div className='lg:grid lg:place-content-center'>
+       {inputs.email.length > 0 && inputs.password.length > 0 ? (
+       <button   className='  button mt-6 grid lg:place-items-center py-3 w-95% sm:w-full lg:w-72 font-semibold text-xl rounded-md text-white '>Sign in</button> ):   <button disabled='true' className='bg-gray-200 grid lg:place-items-center lg:w-72 sm:w-full  mt-6 py-3 w-95% font-semibold text-xl rounded-md text-gray-400 '>Sign in</button> }
+       <p className='text-black text-base font-medium'>Having trouble <span><a className='hover:underline text-base font-normal text-blue-300' href='https://support.shaw.ca/t5/internet-articles/how-to-change-your-shaw-email-password/ta-p/6430'>Shaw Support How To Reset My Password</a></span> </p>
+       </div>
+
+    </form>
+    </div>
+    <div></div>
+    </section>
+
+    
+    </div>
+  );
+}
+export default SignUp;
